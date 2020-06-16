730adec730f23fb5709d271f4e0de33bc3ee1f57
karth <kk@varkasa.com>
2020-06-15 19:16:01 -0700

    made change to test1.java

--- myGitJavaProj1/src/pack1/test1.java
+++ myGitJavaProj1/src/pack1/test1.java
@@ -5,7 +5,11 @@ public class test1 {
 	public static void main(String[] args) {
 		int x = 101, y = 55;
 		int z = x + y;
+		
 		System.out.println(z);
+
+
+		System.out.println("End of the code");
 
 	}
 
