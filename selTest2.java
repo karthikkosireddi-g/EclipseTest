049306a182947615e1e2a06fcfae4ad707a4cc42
karthik kosireddi <58488937+karthikkosireddi-g@users.noreply.github.com>
2020-06-22 19:38:40 -0700

    Update selTest2.java

--- selTest2.java
+++ selTest2.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class MC03_LoanCalc_Basic {
+public class selTest2 {
 
 	public static void main(String[] args) {
 		
