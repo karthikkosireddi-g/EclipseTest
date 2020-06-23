c563cc73421833fe162c8e60261296abce968b3f
karthik kosireddi <58488937+karthikkosireddi-g@users.noreply.github.com>
2020-06-22 19:35:07 -0700

    500 loop

--- forLoop1.java
+++ forLoop1.java
@@ -22,7 +22,7 @@ public class forLoop1 {
 		System.out.println("Value of variable i is " + i);
 		
 		
-		for (int j = 1; j <101; j = j + 1) {
+		for (int j = 1; j <501; j = j + 1) {
 			System.out.println("!!!!!!!!!!!!");
 			System.out.println("Value of j is " + j);
 			System.out.println("!!!!!!!!!!!!");
